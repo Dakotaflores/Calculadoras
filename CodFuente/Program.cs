cfd52d0f56ee4d6c0400b8aac70bd69873b44d5c
Dakotaflores <Dakotaflores981@gmail.com>
2024-04-25 15:34:23 -0400

    Arreglo el codigo por milesima vez

--- CodFuente/Program.cs
+++ CodFuente/Program.cs
@@ -32,65 +32,7 @@ namespace CalculadoraFisica
         }
 
         // Implementa los métodos restantes para MRUA
-    }
-
-    public class Interfaz
-    {
-        private Calculadora calculadora;
-
-        public Interfaz(Calculadora calc)
-        {
-            calculadora = calc;
-        }
-
-        public void MostrarMenu()
-        {
-            Console.WriteLine("1. Calcular distancia (MRU)");
-            Console.WriteLine("2. Calcular velocidad (MRU)");
-            Console.WriteLine("3. Calcular tiempo (MRU)");
-            // Agregar opciones restantes para MRUA
-            Console.WriteLine("7. Terminar programa");
-        }
-
-        public void Ejecutar()
-        {
-            while (true)
-            {
-                MostrarMenu();
-                Console.Write("Ingrese el número de la opción que desea: ");
-                int opcion = int.Parse(Console.ReadLine());
-
-                if (opcion == 1)
-                {
-                    Console.Write("Ingrese la velocidad (m/s): ");
-                    double velocidad = double.Parse(Console.ReadLine());
-                    Console.Write("Ingrese el tiempo (s): ");
-                    double tiempo = double.Parse(Console.ReadLine());
-                    double distancia = calculadora.CalcularDistancia(velocidad, tiempo);
-                    Console.WriteLine($"La distancia es: {distancia} metros.");
-                }
-                else if (opcion == 2)
-                {
-                    // Implementa la funcionalidad para calcular la velocidad
-                }
-                else if (opcion == 3)
-                {
-                    // Implementa la funcionalidad para calcular el tiempo
-                }
-                // Agrega casos restantes para MRUA
-                else if (opcion == 7)
-                {
-                    Console.WriteLine("¡Hasta luego!");
-                    break;
-                }
-                else
-                {
-                    Console.WriteLine("Opción inválida. Por favor, seleccione una opción válida.");
-                }
-            }
-        }
-    }
-}*/
+    }*/
 
 using System;
 
